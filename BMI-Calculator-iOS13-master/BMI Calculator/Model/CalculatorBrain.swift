831fb3069f19595d881e4810782953ef6869e13c
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-21 11:14:35 -0300

    Add Destini and BMI apps
    Destini:
    - MVC Design Pattern practice
    - Review Swift Struct concepts
    
    BMI:
    - MVC Design Pattern pratice
    - Optional concepts
    - Class concepts
    - Comparing Struct vs Class

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Model/CalculatorBrain.swift
@@ -0,0 +1,38 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Rafael Plinio on 20/10/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmiAnswer = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiAnswer
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice."
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .black
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 && bmiValue > 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+        }
+    }
+}
