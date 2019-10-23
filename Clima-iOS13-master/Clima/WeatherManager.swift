380994bcb370ca3f5e2f9619def869f661987689
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-23 15:32:58 -0300

    Update WeatherManager.swift
    Change the url string to be dynamic.
    Using and understanding completionHandler.

--- Clima-iOS13-master/Clima/WeatherManager.swift
+++ Clima-iOS13-master/Clima/WeatherManager.swift
@@ -9,10 +9,38 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=2f55e435cb439c9c7107c8bd54261c91&q=london&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=2f55e435cb439c9c7107c8bd54261c91&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1 - create a URL
+        
+        if let url = URL(string: urlString) {
+            
+            //2  - create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3 - give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            //4 - start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
