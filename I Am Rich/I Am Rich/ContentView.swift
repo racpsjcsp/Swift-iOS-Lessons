2ab7eb04e70859a3692bd56211b5165fe8438090
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-29 14:15:45 -0300

    I Am Rich App
    Introduction to SwiftUI

---
+++ I Am Rich/I Am Rich/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Rafael Plinio on 29/10/19.
+//  Copyright © 2019 Rafael Plinio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
