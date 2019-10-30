b79ceb6b37a7692d58602ddf706af78a2fcfa333
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-30 15:02:45 -0300

    Update ContentView.swift
    Add functionality
    Use of @State (propertyWrapper) to change the value of a property inside a struct. Structs are immutable and the @State is used in SwiftUI to overcome the immutability of a Struct.

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -9,6 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State is a SwiftUI Property - it allows updating the variable, since Structs are immutable.
+    @State var leftDice = 1
+    @State var rightDice = 1
+    
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -18,13 +23,15 @@ struct ContentView: View {
                 Image("diceeLogo")
                 Spacer()
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 1)
+                    DiceView(n: leftDice)
+                    DiceView(n: rightDice)
                 }
                 .padding(.horizontal)
                 Spacer()
                 Button(action: {
-                    
+                    //because the var is changing, need to use @State
+                    self.leftDice = Int.random(in: 1...6)
+                    self.rightDice = Int.random(in: 1...6)
                 }) {
                     Text("Roll")
                         .font(.system(size: 50))
