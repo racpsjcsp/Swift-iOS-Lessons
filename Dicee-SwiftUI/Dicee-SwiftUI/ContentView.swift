979b4f4e4bc7a831232aea9292bf12aa07f58ac4
Rafael Plinio <racpsjcsp@gmail.com>
2019-10-30 14:39:44 -0300

    Dicee (SwiftUI)
    Creating the layout of the Dicee App using SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Rafael Plinio on 30/10/19.
+//  Copyright © 2019 Rafael Plinio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
