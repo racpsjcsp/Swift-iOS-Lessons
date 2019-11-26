3b9e5930ed3cd62bb65e80bdc8d204cdd7c9c7c3
Rafael Plinio <racpsjcsp@gmail.com>
2019-11-26 14:32:57 -0300

    viewDidLoad changed
    replace the original code with CLTypingLabel cocoapod to achieve same result.

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,25 +7,29 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
+        titleLabel.text = "⚡️FlashChat"
         
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
         
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        //no longer needed because of the CLTypingLabel
+        
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
        
     }
     
