dfd8d4cee98ab94b77f1a24278b366103a2cac86
Rafael Plinio <racpsjcsp@gmail.com>
2020-08-15 17:56:08 -0300

    Update timer
    when a button is pressed again, it resets the timer and the timeInterval remains 1.0

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var counter = 0
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
     
-            
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
 //        if (hardness == "Soft") {
@@ -40,7 +43,7 @@ class ViewController: UIViewController {
         
         counter = eggTimes[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         }
     
     @objc func updateCounter() {
