86afd8bf4c9eeb2023c97472404901c749334901
Rafael Plinio <racpsjcsp@gmail.com>
2020-08-15 18:47:20 -0300

    Add Alarm, update progress bar

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,39 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var counter = 0
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
-   
+    var alarm: AVAudioPlayer!
+    
     @IBOutlet weak var doneLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
         
     @IBAction func hardnessSelected(_ sender: UIButton) {
-    
-        timer.invalidate()
+        timer.invalidate() //resets timer
         
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]! //gets int value from the dictionary
         
-        counter = eggTimes[hardness]!
+        progressBar.isHidden = false
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        doneLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
-        progressBar.progress = 1
-        
         }
     
     @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            print("\(counter) seconds")
-            counter -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             doneLabel.text = "DONE!"
+            playAlarm()
+            progressBar.isHidden = true
         }
+    }
+    
+    func playAlarm() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        alarm = try! AVAudioPlayer(contentsOf: url!)
+        alarm.play()
     }
     
 }
