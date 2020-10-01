108887a003c42cf36e9449e2b85d1e977a74a3e0
Rafael Plinio <racpsjcsp@gmail.com>
2020-10-01 20:22:49 -0300

    Update LoginViewController
    Add login functionality (Firebase)
    add warning label in case info is wrong

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                    self.warningLabel.text = e.localizedDescription
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+        }
+        
+            
+        }
     }
     
 }
