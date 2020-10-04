e603f62534154ffec5d69f1106428efd85b18cff
Rafael Plinio <racpsjcsp@gmail.com>
2020-10-03 21:01:28 -0300

    Update ChatViewController, MessageCell.xib
    Add avatar for the other user
    Display avatar for the correspondent user (me, you) with isHidden

--- Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -13,6 +13,7 @@ class MessageCell: UITableViewCell {
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     
 
     //semelhate ao viewDidLoad, mas é para o xib
